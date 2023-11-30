0071e1ccceff2a2277a66a280178d2e0d2c9909e
lazy_owl <slimsly11@gmail.com>
2023-11-30 15:14:12 +0600

    feat(excel): add methods new tab + get tab for excel

--- pkg/excel/public.go
+++ pkg/excel/public.go
@@ -1,5 +1,7 @@
 package excel
 
+import "github.com/lowl11/boostex"
+
 func (excel Excel) SetHeaders(tabName string, headers ...string) error {
 	tab := excel.service.GetTab(tabName)
 	if tab == nil {
@@ -19,6 +21,14 @@ func (excel Excel) SetNatural(tab, index string, value any) error {
 
 func (excel Excel) Create() error {
 	return excel.service.Create()
+}
+
+func (excel Excel) NewTab(tabName string) (boostex.Sheet, error) {
+	return excel.service.NewTab(tabName)
+}
+
+func (excel Excel) GetTab(tabName string) boostex.Sheet {
+	return excel.service.GetTab(tabName)
 }
 
 func (excel Excel) Close() error {
